d8dc6f672391ebe76dce6dc5e7d1c84e417933c1
mamenz752 <mamenz752@gmail.com>
2024-08-08 14:31:12 +0900

    feat: TopPageのデータフェッチング手法変更

--- app/components/BlogItem.tsx
+++ app/components/BlogItem.tsx
@@ -1,43 +1,36 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import React, { FC } from 'react';
-
-interface Image {
-  url: string;
-  height: number;
-  width: number;
-}
+import { client } from '@/libs/client';
 
 type Props = {
   id: string;
-  createdAt: string;
-  updatedAt: string;
-  publishedAt: string;
-  revisedAt: string;
-  topImage: Image;
-  title: string;
-  body: string;
-  categories: string;
 };
 
-const BlogItem: FC<Props> = (props: Props) => {
+const BlogItem: FC<Props> = async (props: Props) => {
+  const data = await client.get({ endpoint: 'blog', contentId: props.id });
+  const article = data.contents;
+  console.log(data);
+
   return (
-    <Link href="/" className="hover:opacity-50">
-      <div className="border border-gray-500">
-        <Image
-          src={props.topImage.url}
-          alt="topImage"
-          width={props.topImage.width}
-          height={props.topImage.height}
-        />
-      </div>
-      <h2 className="my-4 font-bold text-md">{props.title}</h2>
-      <div className="text-right">
-        <p className="inline-block text-sm tracking-wider bg-black text-white mt-2 p-4">
-          {props.categories}
-        </p>
-      </div>
-    </Link>
+    <li>
+      <Link href="/" className="hover:opacity-50">
+        <div className="border border-gray-500">
+          <Image
+            src={data.topImage.url}
+            alt="topImage"
+            width={data.topImage.width}
+            height={data.topImage.height}
+          />
+        </div>
+        <h2 className="h-12 my-4 font-bold text-md">{data.title}</h2>
+        <div className="text-right">
+          <p className="inline-block text-sm tracking-wider bg-black text-white mt-2 p-4">
+            {data.categories}
+          </p>
+        </div>
+      </Link>
+    </li>
   );
 };
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,16 @@
 import { client } from '../libs/client';
 import BlogItem from './components/BlogItem';
 
-interface Image {
-  url: string;
-  height: number;
-  width: number;
-}
-
-interface Article {
-  id: string;
-  createdAt: string;
-  updatedAt: string;
-  publishedAt: string;
-  revisedAt: string;
-  topImage: Image;
-  title: string;
-  body: string;
-  categories: string;
-}
-
 export default async function Home(): Promise<JSX.Element> {
-  const data = await client.get({ endpoint: 'blog' });
-  const articles: Article[] = data.contents;
+  const data = await client.getAllContentIds({ endpoint: 'blog' });
+  const articles = data;
+  console.log(data);
 
   return (
-    <main className="grid grid-cols-3 justify-between py-8">
-      <ul>
-        {articles.map((article: Article, item: number) => (
-          <BlogItem
-            key={item}
-            id={article.id}
-            createdAt={article.createdAt}
-            updatedAt={article.updatedAt}
-            publishedAt={article.publishedAt}
-            revisedAt={article.revisedAt}
-            topImage={article.topImage}
-            title={article.title}
-            body={article.body}
-            categories={article.categories}
-          />
+    <main className="py-8">
+      <ul className="grid grid-cols-3 gap-6">
+        {articles.map((article: string, item: number) => (
+          <BlogItem key={item} id={article} />
         ))}
       </ul>
     </main>

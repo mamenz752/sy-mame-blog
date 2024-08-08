0630feb44230973a4aaab47da68420e5f96b1f6e
mamenz752 <mamenz752@gmail.com>
2024-08-08 12:21:27 +0900

    feat: データフェッチング成功

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,31 @@
-export default function Home() {
+import { GetStaticProps } from 'next';
+import { client } from '../libs/client';
+
+interface Article {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+  topImage: string;
+  title: string;
+  body: string;
+  categories: string;
+}
+
+export default async function Home() {
+  const data = await client.get({ endpoint: 'blog' });
+  const articles = data.contents;
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>sy-mame-blog</h1>
+      <ul>
+        {articles.map((article: Article) => (
+          <li key={article.id}>
+            <a href="">{article.title}</a>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
--- libs/client.ts
+++ libs/client.ts
@@ -1,6 +1,6 @@
 import { createClient } from 'microcms-js-sdk';
 
-const client = createClient({
+export const client = createClient({
   serviceDomain: process.env.SERVICE_DOMAIN || '',
   apiKey: process.env.API_KEY || '',
 });
